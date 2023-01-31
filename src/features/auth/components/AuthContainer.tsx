1fc7fc0d84924c21278bb1ad6ab3c2fb1ce2c1c9
George Kobunov <ancient.gekob@gmail.com>
2023-01-31 09:20:25 +0300

    Improved adaptivity2

--- src/features/auth/components/AuthContainer.tsx
+++ src/features/auth/components/AuthContainer.tsx
@@ -2,6 +2,9 @@ import { Container, styled } from '@nextui-org/react';
 
 const AuthContainer = styled(Container, {
   padding: '2rem 0 !important',
+  '@xsMax': {
+    padding: '1rem 0 !important',
+  },
 });
 
 export default AuthContainer;
