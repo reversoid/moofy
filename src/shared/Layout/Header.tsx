e879e7acf4bce3bd654718b28ee3b6bae778e2fd
George Kobunov <ancient.gekob@gmail.com>
2023-01-31 10:37:21 +0300

    Fixed styles

--- src/shared/Layout/Header.tsx
+++ src/shared/Layout/Header.tsx
@@ -5,12 +5,17 @@ export const HEADER_HEIGHT = '4.75rem';
 
 const HeaderStyled = styled('header', {
   position: 'fixed',
+  width: '100%',
 });
 
 const HeaderContainer = styled(Container, {
   height: HEADER_HEIGHT,
   backdropFilter: 'saturate(180%) blur(10px)',
   boxShadow: 'rgb(2 1 1 / 10%) 0px 5px 20px -5px',
+  '@xsMax': {
+    paddingLeft: '0.5rem !important',
+    paddingRight: '0.5rem !important',
+  },
 });
 
 const CenteredImage = styled(Image, {
