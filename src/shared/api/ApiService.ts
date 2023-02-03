4d3945062e5dc94a1f6b9ea05a258f0e3d628485
George Kobunov <ancient.gekob@gmail.com>
2023-02-03 05:03:37 +0300

    Implement Api service

--- src/shared/api/ApiService.ts
+++ src/shared/api/ApiService.ts
@@ -1,5 +1,10 @@
 import ky, { Options } from 'ky';
 
+export interface ApiError {
+  code: number;
+  message: string;
+}
+
 /** This class provides apiUrl of moofy backend */
 export default class ApiService {
   protected apiUrl = 'http://localhost:3333';
@@ -8,31 +13,65 @@ export default class ApiService {
     relativeUrl: `/${string}`,
     options?: Options,
   ) {
-    return ky.post(this.apiUrl + relativeUrl, options).json<Response>();
+    return this.fetchRetry<Response>(
+      this.apiUrl + relativeUrl,
+      'post',
+      options,
+    );
   }
 
   protected async get<Response = unknown>(
     relativeUrl: `/${string}`,
     options?: Options,
   ) {
-    return ky.get(this.apiUrl + relativeUrl, options).json<Response>();
+    return this.fetchRetry<Response>(this.apiUrl + relativeUrl, 'get', options);
   }
 
   protected async patch<Response = unknown>(
     relativeUrl: `/${string}`,
     options?: Options,
   ) {
-    return ky.patch(this.apiUrl + relativeUrl, options).json<Response>();
+    return this.fetchRetry<Response>(
+      this.apiUrl + relativeUrl,
+      'patch',
+      options,
+    );
   }
 
   protected async delete<Response = unknown>(
     relativeUrl: `/${string}`,
     options?: Options,
   ) {
-    return ky.delete(this.apiUrl + relativeUrl, options).json<Response>();
+    return this.fetchRetry<Response>(
+      this.apiUrl + relativeUrl,
+      'delete',
+      options,
+    );
   }
-}
 
-export interface ApiError {
-  message: string;
+  private async fetchRetry<Response = unknown>(
+    url: string,
+    method: 'get' | 'post' | 'patch' | 'delete',
+    options?: Options,
+  ): Promise<Response> {
+    try {
+      return await ky(url, {
+        ...options,
+        method,
+        retry: {
+          limit: 4,
+          methods: ['post', 'get', 'patch', 'delete'],
+          statusCodes: [408, 413, 429, 500, 502, 503, 504],
+          backoffLimit: 200,
+        },
+      }).json<Response>();
+    } catch (error: any) {
+      if (error.name === 'HTTPError') {
+        const errorJson = await error.response.json();
+
+        throw new Error('Http Error', { cause: errorJson });
+      }
+      throw new Error('Unknown Http Error');
+    }
+  }
 }
