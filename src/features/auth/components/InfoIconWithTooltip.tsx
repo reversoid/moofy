3e242fe963ef4f9c7742006290c00b64cb6608e9
George Kobunov <ancient.gekob@gmail.com>
2023-02-05 02:54:58 +0300

    Replaced icon for error in register page

--- src/features/auth/components/InfoIconWithTooltip.tsx
+++ src/features/auth/components/InfoIconWithTooltip.tsx
@@ -6,7 +6,7 @@ const InfoIconWithTooltip = ({ message }: { message: string }) => {
   return (
     <Tooltip color="invert" content={message} placement="leftStart">
       <Button light css={{ p: 0, m: 0, width: 20, height: 20, minWidth: 0 }}>
-        <Image src={infoIcon} alt="Info icon" width={20} height={20} />
+        <Image src={infoIcon} alt="Info icon" width={'1.5rem'} height={'1.5rem'} />
       </Button>
     </Tooltip>
   );
