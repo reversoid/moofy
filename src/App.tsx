bd434f0c30da519face93807f3531c5c099ff73b
George Kobunov <ancient.gekob@gmail.com>
2023-02-24 16:23:33 +0300

    Replace usemount with useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routes } from './pages/routes';
 import UIProvider from './providers/UIProvider/UIProvider';
-import { Suspense, memo } from 'react';
-import { useMount } from './shared/hooks/useMount';
+import { Suspense, memo, useEffect } from 'react';
 import { appStarted } from './models/app/app';
 
 const router = createBrowserRouter(routes);
 
 function App() {
-  useMount(appStarted);
+  useEffect(appStarted, []);
 
   return (
     <UIProvider>
