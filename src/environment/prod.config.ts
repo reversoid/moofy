08faff44f6b603421e9eb3ff1b4be322202931ad
George Kobunov <ancient.gekob@gmail.com>
2023-03-01 03:39:54 +0300

    Fix prod config

--- src/environment/prod.config.ts
+++ src/environment/prod.config.ts
@@ -1,6 +1,6 @@
 import { Environment } from "./Environment";
 
 export const prodEnv: Environment = {
-  apiUrl: 'http://77.222.42.131:3333',
+  apiUrl: 'https://api.moofy.ru',
   RERENDER_AMOUNT: 1
 };
