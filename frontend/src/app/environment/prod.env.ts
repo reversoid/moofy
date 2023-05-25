6e49523d1decef328bff14046bd51303796cc861
George Kobunov <ancient.gekob@gmail.com>
2023-05-25 16:25:31 +0300

    Try fix frontend

--- frontend/src/app/environment/prod.env.ts
+++ frontend/src/app/environment/prod.env.ts
@@ -1,6 +1,6 @@
 import { Environment } from "./Environment";
 
 export const prodEnv: Environment = {
-  apiUrl: 'https://api.moofy.ru',
+  apiUrl: 'https://api.prod.moofy.ru',
   RERENDER_AMOUNT: 1
 };
