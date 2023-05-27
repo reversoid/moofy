3f30b40004064a2216c338dd3271027352f0c38c
George Kobunov <ancient.gekob@gmail.com>
2023-05-27 21:09:48 +0300

    Fix configs

--- backend/src/config/postgres.config.ts
+++ backend/src/config/postgres.config.ts
@@ -1,21 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
-const getPostgresHost = () => {
-  const isProduction = process.env.NODE_ENV.trim() === 'prod';
-
-  if (isProduction) {
-    const productionHost = process.env.POSTGRES_KNOWN_HOST.trim();
-    return productionHost;
-  }
-
-  const testHost = process.env.POSTGRES_TEST_KNOWN_HOST.trim();
-  return testHost;
-};
-
 export default registerAs('postgres', () => ({
   user: process.env.POSTGRES_USER?.trim(),
   password: process.env.POSTGRES_PASSWORD?.trim(),
-  host: getPostgresHost(),
+  host: process.env.POSTGRES_KNOWN_HOST.trim(),
   port: parseInt(process.env.POSTGRES_PORT?.trim()),
   dbName: process.env.POSTGRES_DB_NAME?.trim(),
 }));
