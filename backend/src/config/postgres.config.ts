7841dac20329b806e266875d01f3f79deadeed8e
George Kobunov <ancient.gekob@gmail.com>
2023-05-31 23:42:09 +0300

    Fix

--- backend/src/config/postgres.config.ts
+++ backend/src/config/postgres.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('postgres', () => ({
   user: process.env.POSTGRES_USER?.trim(),
   password: process.env.POSTGRES_PASSWORD?.trim(),
-  host: process.env.POSTGRES_KNOWN_HOST.trim(),
+  host: process.env.POSTGRES_KNOWN_HOST?.trim(),
   port: parseInt(process.env.POSTGRES_PORT?.trim()),
   dbName: process.env.POSTGRES_DB_NAME?.trim(),
 }));
