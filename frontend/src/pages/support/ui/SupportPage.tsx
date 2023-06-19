f94e07fa9ea6a67039efc9c0fad11623b099d225
George Kobunov <ancient.gekob@gmail.com>
2023-06-19 23:58:48 +0300

    Styled support page

--- frontend/src/pages/support/ui/SupportPage.tsx
+++ frontend/src/pages/support/ui/SupportPage.tsx
@@ -1,4 +1,13 @@
-import { Link, Text } from '@nextui-org/react';
+import { Link, Text, styled } from '@nextui-org/react';
+
+const Ul = styled('ul', {
+  m: 0,
+  mt: '$8',
+});
+
+const Li = styled('li', {
+  m: 0,
+});
 
 const SupportPage = () => {
   return (
@@ -6,19 +15,27 @@ const SupportPage = () => {
       <Text h1>Поддержка</Text>
       <Text>
         Если вы столкнулись с проблемой или у вас есть предложение по улучшению
-        сервиса — пишите на почту{'  '}
-        <Link css={{ fontSize: '$lg' }} href="mailto:grenka7777777@gmail.com">
-          grenka7777777@gmail.com
-        </Link>{' '}
-        или Telegram{' '}
-        <Link
-          css={{ fontSize: '$lg' }}
-          href="https://t.me/xoxo_gosha"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          @xoxo_gosha
-        </Link>
+        сервиса — пишите:
+        <Ul>
+          <Li>
+            <Link
+              css={{ fontSize: '$xl' }}
+              href="mailto:grenka7777777@gmail.com"
+            >
+              grenka7777777@gmail.com
+            </Link>
+          </Li>
+          <Li>
+            <Link
+              css={{ fontSize: '$xl' }}
+              href="https://t.me/xoxo_gosha"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @xoxo_gosha
+            </Link>
+          </Li>
+        </Ul>
       </Text>
     </>
   );
