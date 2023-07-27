be09fef556bb8159d1badcf2dc9f7b91946574f4
George Kobunov <ancient.gekob@gmail.com>
2023-07-27 15:51:03 +0300

    Fix file App.tsx naming

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Application from './_App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Application />
   </React.StrictMode>,
-)
+);
