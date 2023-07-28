870fdea4b8521a11ba79b5e52f4b9dc1022184ab
George Kobunov <ancient.gekob@gmail.com>
2023-07-28 16:23:15 +0300

    Left describing comment for hook

--- frontend/src/shared/hooks/useMount.tsx
+++ frontend/src/shared/hooks/useMount.tsx
@@ -3,6 +3,7 @@ import { DependencyList, EffectCallback, useEffect, useRef } from 'react';
 
 const RERENDERS_AMOUNT = environment.RERENDER_AMOUNT;
 
+/** Helps to avoid **React.Strictmode** double rerender */
 export const useMount = (callback: EffectCallback, deps?: DependencyList) => {
   const rerendersCount = useRef(1);
 
