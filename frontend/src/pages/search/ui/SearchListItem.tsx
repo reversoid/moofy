ab7f04838f37111f76db0f98f16e691790922f5e
George Kobunov <ancient.gekob@gmail.com>
2023-07-29 01:07:06 +0300

    Made some refactoring

--- frontend/src/pages/search/ui/SearchListItem.tsx
+++ frontend/src/pages/search/ui/SearchListItem.tsx
@@ -1,76 +1,61 @@
 import { colorHash } from '@/entities/List/ui/List/ListBackground';
+import {
+  Img,
+  ImgPlaceholder,
+  SearchItem,
+} from '@/entities/search-item/SearchItem';
 import { List } from '@/shared/api/types/list.type';
-import { Card } from '@/shared/ui/Card';
 import { Link } from '@/shared/ui/Link/Link';
-import { Image, Text, styled } from '@nextui-org/react';
-import React, { FC } from 'react';
+import { Text } from '@nextui-org/react';
+import { FC } from 'react';
 
 export interface SearchListItemProps {
   list: List;
 }
 
-const Content = styled('div', { flexGrow: 1 });
+const Image: FC<{ url?: string; valueToHash: string }> = ({
+  url,
+  valueToHash,
+}) => {
+  return url ? (
+    <Img src={url} largerOnXs />
+  ) : (
+    <ImgPlaceholder
+      css={{
+        backgroundColor: colorHash.hex(valueToHash),
+        '@xsMax': {
+          mx: 'auto',
+        },
+      }}
+      largerOnXs
+    />
+  );
+};
 
-const ImgWrapper = styled('div', {
-  display: 'flex',
-  ai: 'center',
-  jc: 'center',
-});
-
-const Img = styled(Image, {
-  margin: 0,
-  img: {
-    objectFit: 'cover',
-  },
-  flexShrink: 0,
-});
-
-const MainContent = styled('div', {
-  display: 'flex',
-  jc: 'space-between',
-  flexDirection: 'column',
-  height: '100%',
-});
-
-const Background = styled('div', {
-  width: '7rem',
-  height: '7rem',
-});
+const Creator: FC<{ username: string; id: number }> = ({ id, username }) => {
+  return (
+    <Text as={'p'} css={{ mt: '$2', color: '$textLight', marginTop: 'auto' }}>
+      Создатель{' '}
+      <Link
+        css={{ fontWeight: '$medium', display: 'inline' }}
+        to={`/profile/${id}`}
+      >
+        {username}
+      </Link>
+    </Text>
+  );
+};
 
 export const SearchListItem: FC<SearchListItemProps> = ({ list }) => {
   return (
-    <Card horizontal>
-      <ImgWrapper>
-        <Link to={`/list/${list.id}`}>
-          {list.image_url ? (
-            <Img width={'7rem'} height={'7rem'} src={list.image_url ?? ''} />
-          ) : (
-            <Background css={{backgroundColor: colorHash.hex(String(list.id))}}/>
-          )}
-        </Link>
-      </ImgWrapper>
-      <Content>
-        <MainContent>
-          <div>
-            <Link to={`/list/${list.id}`}>
-              <Text h4 color="$primary" css={{ lineHeight: '$sm' }}>
-                {list.name}
-              </Text>
-            </Link>
-
-            <Text as={'p'}>{list.description}</Text>
-          </div>
-          <Text as={'p'} css={{ mt: '$2', color: '$textLight' }}>
-            Создатель{' '}
-            <Link
-              css={{ fontWeight: '$medium', display: 'inline' }}
-              to={`/profile/${list.user.id}`}
-            >
-              {list.user.username}
-            </Link>
-          </Text>
-        </MainContent>
-      </Content>
-    </Card>
+    <SearchItem
+      title={list.name}
+      description={list.description}
+      link={`/list/${list.id}`}
+      image={<Image valueToHash={`${list.id}`} url={list.image_url} />}
+      bottomContent={
+        <Creator id={list.user.id} username={list.user.username} />
+      }
+    />
   );
 };
--- frontend/src/pages/search/ui/SearchProfileItem.tsx
+++ frontend/src/pages/search/ui/SearchProfileItem.tsx
@@ -1,43 +1,19 @@
+import {
+  Img,
+  ImgPlaceholder,
+  SearchItem,
+} from '@/entities/search-item/SearchItem';
 import { ProfileShortWithDescription } from '@/shared/api/types/profile.type';
-import { Card } from '@/shared/ui/Card';
-import { Link } from '@/shared/ui/Link/Link';
-import { Image, Text, styled } from '@nextui-org/react';
-import { FC } from 'react';
-import profileIcon from '@/shared/assets/img/user-round.svg'
+import profileIcon from '@/shared/assets/img/user-round.svg';
 import { Icon } from '@/shared/ui/Icon/Icon';
+import { styled } from '@nextui-org/react';
+import { FC } from 'react';
 
 export interface SearchProfileItemProps {
   profile: ProfileShortWithDescription;
 }
 
-// TODO share with search list item
-
-const Content = styled('div', { flexGrow: 1 });
-
-const ImgWrapper = styled('div', {
-  display: 'flex',
-  ai: 'center',
-  jc: 'center',
-});
-
-const Img = styled(Image, {
-  margin: 0,
-  img: {
-    objectFit: 'cover',
-  },
-  flexShrink: 0,
-  borderRadius: '50%',
-});
-
-const MainContent = styled('div', {
-  display: 'flex',
-  flexDirection: 'column',
-  height: '100%',
-});
-
-const Background = styled('div', {
-  width: '7rem',
-  height: '7rem',
+const Background = styled(ImgPlaceholder, {
   backgroundColor: '$neutral',
   borderRadius: '50%',
   display: 'flex',
@@ -45,34 +21,29 @@ const Background = styled('div', {
   jc: 'center',
 });
 
+const ImgRounded = styled(Img, {
+  borderRadius: '50%',
+});
+
+const Image: FC<{ url: string | null }> = ({ url }) => {
+  if (url) {
+    return <ImgRounded src={url} smallerOnXs />;
+  }
+
+  return (
+    <Background smallerOnXs>
+      <Icon size={'3.5rem'} iconUrl={profileIcon} />
+    </Background>
+  );
+};
+
 export const SearchProfileItem: FC<SearchProfileItemProps> = ({ profile }) => {
   return (
-    <Card horizontal>
-      <ImgWrapper>
-        <Link to={`/profile/${profile.id}`}>
-          {profile.image_url ? (
-            <Img width={'7rem'} height={'7rem'} src={profile.image_url ?? ''} />
-          ) : (
-            <Background>
-              <Icon
-                size={'3.5rem'}
-                iconUrl={profileIcon}
-              />
-            </Background>
-          )}
-        </Link>
-      </ImgWrapper>
-      <Content>
-        <MainContent>
-          <Link to={`/profile/${profile.id}`}>
-            <Text h4 color="$primary" css={{ lineHeight: '$sm' }}>
-              {profile.username}
-            </Text>
-          </Link>
-
-          <Text as={'p'}>{profile.description}</Text>
-        </MainContent>
-      </Content>
-    </Card>
+    <SearchItem
+      title={profile.username}
+      description={profile.description ?? ''}
+      link={`/profile/${profile.id}`}
+      image={<Image url={profile.image_url} />}
+    />
   );
 };
