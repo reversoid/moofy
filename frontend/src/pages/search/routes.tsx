9e0eb5a0e5f77ef8bab3c84dd362efdfc4598cad
George Kobunov <ancient.gekob@gmail.com>
2023-08-19 14:10:05 +0300

    Fix routing

--- frontend/src/pages/search/routes.tsx
+++ frontend/src/pages/search/routes.tsx
@@ -1,10 +1,11 @@
-import { RouteObject } from 'react-router-dom';
+import { RouteObject, Navigate } from 'react-router-dom';
 import { lazy } from 'react';
 
-const SearchCollections = lazy(() => import('./ui/subpages/SearchCollections'))
-const SearchProfiles = lazy(() => import('./ui/subpages/SearchProfiles'))
+const SearchCollections = lazy(() => import('./ui/subpages/SearchCollections'));
+const SearchProfiles = lazy(() => import('./ui/subpages/SearchProfiles'));
 
 export const routes: RouteObject[] = [
   { path: 'collections', element: <SearchCollections /> },
   { path: 'profiles', element: <SearchProfiles /> },
+  { path: '*', element: <Navigate to={'/search/collections'} /> },
 ];
