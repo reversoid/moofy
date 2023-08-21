5f57b5724ae8f964c01faff5b841e0f2aabe2c2b
George Kobunov <ancient.gekob@gmail.com>
2023-08-21 08:52:48 +0300

    Fix

--- frontend/src/pages/welcome/UpdatesPage/index.tsx
+++ frontend/src/pages/welcome/UpdatesPage/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useUpdates } from './utils/useUpdates';
+import { useLoadingBar } from '@/shared/hooks/useLoadingBar';
 
 export const UpdatesPage = () => {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
+    useUpdates();
+
+  useLoadingBar(isLoading);
+
   return <div>Some updates here</div>;
 };
 
