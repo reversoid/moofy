8c878ea2cb1ab361069193cf50390a2308b77ea3
George Kobunov <ancient.gekob@gmail.com>
2023-09-06 13:59:07 +0300

    Refactored comment entity

--- frontend/src/entities/comment/index.tsx
+++ frontend/src/entities/comment/index.tsx
@@ -1,69 +1,15 @@
 import { Profile } from '@/shared/api/types/profile.type';
-import { Image, Text, styled } from '@nextui-org/react';
+import { Text, styled } from '@nextui-org/react';
 import { FC } from 'react';
 import { Card } from '../../shared/ui/Card';
-import { Heart } from '@/shared/ui/Heart';
-import profileIcon from '@/shared/assets/img/user-round.svg';
-import { Link } from '@/shared/ui/Link/Link';
-import { formatDate } from '@/shared/lib/formatDate/formatDate';
+import { User } from './ui/user';
 
 export interface CommentProps {
   text: string;
   user: Pick<Profile, 'id' | 'image_url' | 'username'>;
   createdAt: Date;
-  like?: {
-    onCommentLikeChange: (liked: boolean) => void;
-    liked: boolean;
-    loading: boolean;
-  };
   replyToCommentId?: number;
 }
-
-const UserBlock = styled('div', {
-  display: 'flex',
-  ai: 'center',
-  gap: '$6',
-});
-
-const UserInfo = styled('div', {
-  display: 'flex',
-  flexDirection: 'column',
-  jc: 'space-between',
-  ai: 'flex-start',
-  flexGrow: 2,
-});
-
-interface UserProps {
-  user: Pick<Profile, 'id' | 'image_url' | 'username'>;
-  createdAt: Date;
-}
-
-export const User: FC<UserProps> = ({ user, createdAt }) => {
-  return (
-    <UserBlock>
-      <Link to={`/profile/${user.id}`} css={{ width: 'fit-content' }}>
-        <Image
-          width={'2.75rem'}
-          height={'2.75rem'}
-          css={{
-            borderRadius: '50%',
-            objectFit: 'cover',
-          }}
-          src={user.image_url ?? profileIcon}
-        />
-      </Link>
-
-      <UserInfo>
-        <Link to={`/profile/${user.id}`} css={{ width: 'fit-content' }}>
-          <Text h5 color="$link" css={{ margin: 0 }} size={'$lg'}>
-            {user.username}
-          </Text>
-        </Link>
-        <Text color="$neutral">{formatDate(createdAt)}</Text>
-      </UserInfo>
-    </UserBlock>
-  );
-};
 
 const CommentWrapper = styled(Card, {
   px: '$sm',
@@ -83,7 +29,6 @@ const CommentWrapper = styled(Card, {
 export const Comment: FC<CommentProps> = ({
   text,
   user,
-  like,
   createdAt,
   replyToCommentId,
 }) => {
@@ -91,15 +36,6 @@ export const Comment: FC<CommentProps> = ({
     <CommentWrapper reply={Boolean(replyToCommentId)}>
       <User user={user} createdAt={createdAt} />
       <Text>{text}</Text>
-
-      {/* TODO should heart be here? */}
-      {like && (
-        <Heart
-          liked={like.liked}
-          loading={like.loading}
-          onChange={like.onCommentLikeChange}
-        />
-      )}
     </CommentWrapper>
   );
 };
---
+++ frontend/src/entities/comment/ui/user.tsx
@@ -0,0 +1,54 @@
+import { Profile } from '@/shared/api/types/profile.type';
+import { Link } from '@/shared/ui/Link/Link';
+import { Image, Text, styled } from '@nextui-org/react';
+import { FC } from 'react';
+import profileIcon from '@/shared/assets/img/user-round.svg';
+import { formatDate } from '@/shared/lib/formatDate/formatDate';
+
+const UserBlock = styled('div', {
+  display: 'flex',
+  ai: 'center',
+  gap: '$6',
+});
+
+const UserInfo = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  jc: 'space-between',
+  ai: 'flex-start',
+  flexGrow: 2,
+});
+
+interface UserProps {
+  user: Pick<Profile, 'id' | 'image_url' | 'username'>;
+  createdAt: Date;
+}
+
+export const User: FC<UserProps> = ({ user, createdAt }) => {
+  return (
+    <UserBlock>
+      <Link to={`/profile/${user.id}`} css={{ width: 'fit-content' }}>
+        <Image
+          width={'2.75rem'}
+          height={'2.75rem'}
+          css={{
+            borderRadius: '50%',
+            objectFit: 'cover',
+          }}
+          src={user.image_url ?? profileIcon}
+        />
+      </Link>
+
+      <UserInfo>
+        <Link to={`/profile/${user.id}`} css={{ width: 'fit-content' }}>
+          <Text h5 color="$link" css={{ margin: 0 }} size={'$lg'}>
+            {user.username}
+          </Text>
+        </Link>
+
+        {/* TODO make better formatting with: 5 min ago. Just now... 15 min ago or 12:12 10.10.2023 */}
+        <Text color="$neutral">{formatDate(createdAt)}</Text>
+      </UserInfo>
+    </UserBlock>
+  );
+};
