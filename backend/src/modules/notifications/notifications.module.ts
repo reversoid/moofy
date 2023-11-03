221d3dad1321d3b2de56614b7a7a18e5e86a17fd
George Kobunov <georgijkobunov@MacBook-Pro-Georgij.local>
2023-11-03 19:54:56 +0300

    Add rabbitmq to docker compose dev file

--- backend/src/modules/notifications/notifications.module.ts
+++ backend/src/modules/notifications/notifications.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from './entities/event.entity';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
+import { EventRepository } from './repositories/event.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Event])],
   controllers: [NotificationsController],
-  providers: [NotificationsService],
+  providers: [NotificationsService, EventRepository],
 })
 export class NotificationsModule {}
