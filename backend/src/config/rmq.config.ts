751dc170aebb14a351a151af3fefe29a4ecf3ff0
George Kobunov <georgijkobunov@MacBook-Pro-Georgij.local>
2023-11-07 23:09:52 +0300

    Updated cicd pipeline and docker for rmq

--- backend/src/config/rmq.config.ts
+++ backend/src/config/rmq.config.ts
@@ -1,10 +1,10 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('rmq', () => ({
-  user: process.env.RABBITMQ_USER?.trim(),
-  password: process.env.RABBITMQ_PASS?.trim(),
-  port: parseInt(process.env.RABBITMQ_PORT?.trim()),
-  exchangeName: process.env.RABBITMQ_EXCHANGE?.trim(),
-  host: process.env.RABBITMQ_HOST?.trim(),
-  queueName: process.env.RABBITMQ_QUEUE_NAME?.trim(),
+  user: process.env.RMQ_USER?.trim(),
+  password: process.env.RMQ_PASS?.trim(),
+  port: parseInt(process.env.RMQ_PORT?.trim()),
+  exchangeName: process.env.RMQ_EXCHANGE?.trim(),
+  host: process.env.RMQ_KNOWN_HOST?.trim(),
+  queueName: process.env.RMQ_QUEUE_NAME?.trim(),
 }));
