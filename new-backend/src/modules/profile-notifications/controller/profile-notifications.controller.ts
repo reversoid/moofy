6c8e4bf7620f8e9918cc723880c500ec94daecfe
George Kobunov <georgijkobunov@MacBook-Pro-Georgij.local>
2023-12-14 20:51:30 +0300

    Left todo

--- new-backend/src/modules/profile-notifications/controller/profile-notifications.controller.ts
+++ new-backend/src/modules/profile-notifications/controller/profile-notifications.controller.ts
@@ -100,6 +100,7 @@ export class ProfileNotificationsController
     this.rmqService.notify(PROFILE_SEEN_NOTIFICATION_TOPIC, notification);
   }
 
+  // TODO if needed, it should use several topics
   @RMQRoute(CREATE_USER_EVENT_TOPIC)
   async createNotification(message: UserEvent) {
     if (!isAllowedProfileEvent(message)) {
