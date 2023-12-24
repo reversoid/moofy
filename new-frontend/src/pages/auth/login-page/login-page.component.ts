55686cd309b0410afdb5a822920a08177d74e4a8
George Kobunov <georgijkobunov@MacBook-Pro-Georgij.local>
2023-12-24 23:28:01 +0300

    check how page looks

--- new-frontend/src/pages/auth/login-page/login-page.component.ts
+++ new-frontend/src/pages/auth/login-page/login-page.component.ts
@@ -1,13 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiButtonModule } from '@taiga-ui/core';
+import { TuiInputModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [TuiInputModule, TuiButtonModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginPageComponent {
-
-}
+export class LoginPageComponent {}
