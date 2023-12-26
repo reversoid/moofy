5ab0d48b3f35d6e1ad4413a14ed10ec6378dfdca
George Kobunov <georgijkobunov@MacBook-Pro-Georgij.local>
2023-12-26 21:27:58 +0300

    Some learn of dropdown

--- new-frontend/src/app/ui/header/header.component.ts
+++ new-frontend/src/app/ui/header/header.component.ts
@@ -1,12 +1,34 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiButtonModule, TuiDropdownModule } from '@taiga-ui/core';
 import { TuiAvatarModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, TuiButtonModule, TuiDropdownModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  open = false;
+
+  onClick(): void {
+    this.open = !this.open;
+  }
+
+  onObscured(obscured: any): void {
+    console.log(obscured);
+
+    if (obscured) {
+      this.open = false;
+    }
+  }
+
+  onActiveZone(active: any): void {
+    console.log(active);
+
+    this.open = active && this.open;
+  }
+}
