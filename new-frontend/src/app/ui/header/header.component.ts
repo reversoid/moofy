c8cde7b9609394ad63f0ac1515920b87932f98be
George Kobunov <georgijkobunov@MacBook-Pro-Georgij.local>
2023-12-26 20:48:04 +0300

    Implement header

--- new-frontend/src/app/ui/header/header.component.ts
+++ new-frontend/src/app/ui/header/header.component.ts
@@ -1,12 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { TuiAvatarModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
