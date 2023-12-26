66c1f317900862dfc5a1e4b719801b4b58cb3d9a
George Kobunov <georgijkobunov@MacBook-Pro-Georgij.local>
2023-12-26 20:22:03 +0300

    Implement auth pages

--- new-frontend/src/pages/auth/login-page/login-page.component.ts
+++ new-frontend/src/pages/auth/login-page/login-page.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { TuiButtonModule } from '@taiga-ui/core';
+import { RouterModule } from '@angular/router';
+import { TuiButtonModule, TuiLinkModule } from '@taiga-ui/core';
 import { TuiInputModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [TuiInputModule, TuiButtonModule],
+  imports: [TuiInputModule, TuiButtonModule, RouterModule, TuiLinkModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- new-frontend/src/pages/auth/register-page/register-page.component.ts
+++ new-frontend/src/pages/auth/register-page/register-page.component.ts
@@ -1,13 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TuiButtonModule } from '@taiga-ui/core';
+import { TuiInputModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [TuiInputModule, TuiButtonModule, RouterModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterPageComponent {
-
-}
+export class RegisterPageComponent {}
