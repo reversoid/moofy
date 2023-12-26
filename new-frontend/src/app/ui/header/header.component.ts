ab7709fe12db31c8898c4b1b34e62e16a12d8180
George Kobunov <georgijkobunov@MacBook-Pro-Georgij.local>
2023-12-26 21:40:36 +0300

    Fix header

--- new-frontend/src/app/ui/header/header.component.ts
+++ new-frontend/src/app/ui/header/header.component.ts
@@ -1,12 +1,17 @@
 import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiActiveZoneModule } from '@taiga-ui/cdk';
 import { TuiButtonModule, TuiDropdownModule } from '@taiga-ui/core';
-import { TuiAvatarModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgOptimizedImage, TuiButtonModule, TuiDropdownModule],
+  imports: [
+    NgOptimizedImage,
+    TuiButtonModule,
+    TuiDropdownModule,
+    TuiActiveZoneModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,16 +24,12 @@ export class HeaderComponent {
   }
 
   onObscured(obscured: any): void {
-    console.log(obscured);
-
     if (obscured) {
       this.open = false;
     }
   }
 
-  onActiveZone(active: any): void {
-    console.log(active);
-
+  onActiveZone(active: boolean): void {
     this.open = active && this.open;
   }
 }
