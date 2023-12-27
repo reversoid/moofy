a711ebfa6f83e40c02692af6f953bb6e66c481d0
George Kobunov <georgijkobunov@MacBook-Pro-Georgij.local>
2023-12-27 23:14:46 +0300

    Add search for collections

--- new-frontend/src/pages/collection-page/collection-page.component.ts
+++ new-frontend/src/pages/collection-page/collection-page.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CollectionComponent } from '../../entities/collection/collection.component';
+import { TuiInputModule } from '@taiga-ui/kit';
+import { TuiLabelModule, TuiSvgModule } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-collection-page',
   standalone: true,
-  imports: [CollectionComponent],
+  imports: [CollectionComponent, TuiInputModule, TuiLabelModule, TuiSvgModule],
   templateUrl: './collection-page.component.html',
   styleUrl: './collection-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
