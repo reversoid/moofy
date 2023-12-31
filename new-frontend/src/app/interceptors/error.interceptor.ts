647d46dacf1bc0dc5bcfe256153b6fe3f481346c
George Kobunov <georgijkobunov@MacBook-Pro-Georgij.local>
2023-12-31 19:46:21 +0300

    Implement error interceptor

--- new-frontend/src/app/interceptors/error.interceptor.ts
+++ new-frontend/src/app/interceptors/error.interceptor.ts
@@ -7,13 +7,39 @@ import { inject } from '@angular/core';
 import { NotificationService } from '../utils/notification.service';
 import { tap } from 'rxjs';
 
+interface ApiError {
+  message: string;
+  statusCode: number;
+}
+
+const isApiError = (value: any): value is ApiError => {
+  return (
+    typeof value === 'object' &&
+    typeof value.statusCode === 'number' &&
+    typeof value.message === 'string'
+  );
+};
+
+// TODO fill messages
+const ERROR_TRANSLATION: Record<string, string> = {};
+
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  const authService = inject(NotificationService);
+  const notificationService = inject(NotificationService);
+
   return next(req).pipe(
     tap({
       error(err) {
-        if (err instanceof HttpErrorResponse) {
+        if (!(err instanceof HttpErrorResponse)) {
+          return;
         }
+        if (!isApiError(err.error)) {
+          return;
+        }
+        const translation = ERROR_TRANSLATION[err.error.message];
+        if (!translation) {
+          return;
+        }
+        notificationService.createNotification(translation);
       },
     })
   );
