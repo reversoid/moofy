3673521383470f88591cdb731c8893e8f6bebe98
George Kobunov <reversoid@MacBook-Pro-Georgij.local>
2024-01-07 15:19:04 +0300

    Implement loader for collrction grid

--- new-frontend/src/widgets/collection-grid/collection-grid.component.ts
+++ new-frontend/src/widgets/collection-grid/collection-grid.component.ts
@@ -1,12 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CollectionComponent } from '../../entities/collection/collection.component';
+import { TuiLoaderModule } from '@taiga-ui/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-collection-grid',
   standalone: true,
-  imports: [CollectionComponent],
+  imports: [NgIf, CollectionComponent, TuiLoaderModule],
   templateUrl: './collection-grid.component.html',
   styleUrl: './collection-grid.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CollectionGridComponent {}
+export class CollectionGridComponent {
+  loading = true;
+}
