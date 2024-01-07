21e6efd04763b2c1e95ca366fe8c7848b6b1f22c
George Kobunov <reversoid@MacBook-Pro-Georgij.local>
2024-01-07 15:33:57 +0300

    Implement notifications

--- new-frontend/src/app/ui/layout/layout.component.ts
+++ new-frontend/src/app/ui/layout/layout.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { WrapperComponent } from '../wrapper/wrapper.component';
+import { NotificationService } from '../../utils/notification.service';
+import { TuiAlertService } from '@taiga-ui/core';
+import { mergeMap, takeUntil } from 'rxjs';
+import { TuiDestroyService } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'app-layout',
@@ -10,5 +14,24 @@ import { WrapperComponent } from '../wrapper/wrapper.component';
   imports: [HeaderComponent, WrapperComponent, FooterComponent, RouterModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
+  providers: [TuiDestroyService],
 })
-export class LayoutComponent {}
+export class LayoutComponent implements OnInit {
+  constructor(
+    private readonly alert: TuiAlertService,
+    private readonly notificationService: NotificationService,
+    private readonly _destroy$: TuiDestroyService,
+  ) {}
+
+  ngOnInit(): void {
+    this.notificationService.errors$
+      .pipe(mergeMap((message) => this.alert.open(message, { status: 'error' })))
+      .pipe(takeUntil(this._destroy$))
+      .subscribe();
+
+    this.notificationService.notifications$
+      .pipe(mergeMap((message) => this.alert.open(message, { status: 'info' })))
+      .pipe(takeUntil(this._destroy$))
+      .subscribe();
+  }
+}
