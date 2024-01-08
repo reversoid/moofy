7d36e3815efe10f2a980ed36f3ea0915ac32bce9
George Kobunov <reversoid@MacBook-Pro-Georgij.local>
2024-01-08 16:42:59 +0300

    Add subscribe and report buttons

--- new-frontend/src/pages/profile-page/profile-page.component.ts
+++ new-frontend/src/pages/profile-page/profile-page.component.ts
@@ -3,11 +3,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TuiIslandModule } from '@taiga-ui/kit';
 import { CollectionGridComponent } from '../../widgets/collection-grid/collection-grid.component';
+import { TuiButtonModule } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [TuiIslandModule, NgOptimizedImage, RouterModule, CollectionGridComponent],
+  imports: [
+    TuiIslandModule,
+    NgOptimizedImage,
+    RouterModule,
+    CollectionGridComponent,
+    TuiButtonModule,
+  ],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
