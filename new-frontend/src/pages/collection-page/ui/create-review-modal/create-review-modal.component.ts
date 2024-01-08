dcddef3153eb42015cc1860ae693fb356c100d39
George Kobunov <reversoid@MacBook-Pro-Georgij.local>
2024-01-08 20:52:57 +0300

    Implement create review flow

--- new-frontend/src/pages/collection-page/ui/create-review-modal/create-review-modal.component.ts
+++ new-frontend/src/pages/collection-page/ui/create-review-modal/create-review-modal.component.ts
@@ -2,9 +2,20 @@ import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TuiLetModule } from '@taiga-ui/cdk';
 import { TuiButtonModule, TuiDataListModule, TuiTextfieldControllerModule } from '@taiga-ui/core';
-import { TuiElasticContainerModule, TuiInputModule } from '@taiga-ui/kit';
+import {
+  TuiElasticContainerModule,
+  TuiInputModule,
+  TuiRatingModule,
+  TuiTextareaModule,
+} from '@taiga-ui/kit';
 import { of } from 'rxjs';
 
+enum CreateReviewSteps {
+  selectFilm,
+  scoreAndDescription,
+}
+
+// TODO make it lazy routable
 @Component({
   selector: 'app-create-review-modal',
   standalone: true,
@@ -18,6 +29,8 @@ import { of } from 'rxjs';
     NgForOf,
     TuiButtonModule,
     TuiElasticContainerModule,
+    TuiTextareaModule,
+    TuiRatingModule,
   ],
   templateUrl: './create-review-modal.component.html',
   styleUrl: './create-review-modal.component.scss',
@@ -35,4 +48,20 @@ export class CreateReviewModalComponent {
   select(user: any) {
     this.user = user;
   }
+
+  step: CreateReviewSteps = CreateReviewSteps.selectFilm;
+
+  get isSelectFilmStep() {
+    return this.step === CreateReviewSteps.selectFilm;
+  }
+
+  get isScoreAndDescriptionStep() {
+    return this.step === CreateReviewSteps.scoreAndDescription;
+  }
+
+  finishSelectFilmStep() {
+    this.step = CreateReviewSteps.scoreAndDescription;
+  }
+
+  createReview() {}
 }
