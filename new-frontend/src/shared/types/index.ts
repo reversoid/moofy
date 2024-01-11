cdc7a6e948fdd380bc1a0cfc0ecd0920ba1e7ab2
George Kobunov <reversoid@MacBook-Pro-Georgij.local>
2024-01-11 23:22:22 +0300

    Add types for api

---
+++ new-frontend/src/shared/types/index.ts
@@ -0,0 +1,89 @@
+export interface PaginatedData<T> {
+  nextKey: string | null;
+  items: T[];
+}
+
+export interface User {
+  id: number;
+  username: string;
+  description: string | null;
+  imageUrl: string | null;
+  createdAt: string;
+}
+
+export interface Profile {
+  user: User;
+  additionalInfo: {
+    isSubscribed: boolean;
+  };
+  socialStats: {
+    followers: number;
+    followees: number;
+  };
+  personalReviewsAmount: number;
+  collections: PaginatedData<Collection>;
+}
+
+export type ShortProfile = Pick<Profile, 'user' | 'additionalInfo'>;
+
+export interface Collection {
+  id: number;
+  name: string;
+  imageUrl: string | null;
+  description: string | null;
+  createdAt: string;
+  updatedAt: string;
+  user: User;
+  isPublic: boolean;
+}
+
+export interface CollectionWithInfo {
+  collection: Collection;
+  socialStats: {
+    likesAmount: number;
+    commentsAmount: number;
+  };
+  additionalInfo: {
+    isFavorite: boolean;
+    isLiked: boolean;
+  };
+}
+
+export interface Film {
+  id: string;
+  name: string;
+  year: number;
+  posterPreviewUrl: string;
+  posterUrl: string;
+  genres: string[];
+  type: 'FILM' | 'TV_SERIES' | 'TV_SHOW' | 'MINI_SERIES' | 'VIDEO';
+}
+
+export interface Review {
+  id: number;
+  score: number | null;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+  film: Film;
+  isHidden: boolean;
+}
+
+export interface Comment {
+  id: number;
+  text: string;
+  user: User;
+  createdAt: string;
+  replyToId: number | null;
+}
+
+export interface CommentWithInfo {
+  comment: Comment;
+  socialStats: {
+    likesAmount: number;
+    repliesAmount: number;
+  };
+  additionalInfo: {
+    isLiked: boolean;
+  };
+}
