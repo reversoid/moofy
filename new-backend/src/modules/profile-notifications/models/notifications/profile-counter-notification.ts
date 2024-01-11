372b62c5efc0cd176f7a161afcfde87c04237b2b
George Kobunov <reversoid@MacBook-Pro-Georgij.local>
2024-01-12 00:56:13 +0300

    Fix type

--- new-backend/src/modules/profile-notifications/models/notifications/profile-counter-notification.ts
+++ new-backend/src/modules/profile-notifications/models/notifications/profile-counter-notification.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const profileCounterNotificationSchema = z.object({
-  eventId: z.string(),
+  notificationId: z.string(),
 });
 
 export type ProfileCounterNotification = z.infer<
