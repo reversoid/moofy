af291b1c157645341f544041bf2436f04ff14f93
reversoid <ancient.gekob@gmail.com>
2024-01-20 23:18:17 +0300

    Create some placeholder components

---
+++ new-frontend/src/entities/user-card/user-card.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-card.component.html',
+  styleUrl: './user-card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UserCardComponent {
+
+}
