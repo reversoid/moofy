92021f0983cfe07726f44a90cdfc46b76b6a18b0
reversoid <ancient.gekob@gmail.com>
2024-01-22 21:10:33 +0300

    Implement collections explore

--- new-frontend/src/pages/explore-page/explore-collections-page/explore-collections-page.component.ts
+++ new-frontend/src/pages/explore-page/explore-collections-page/explore-collections-page.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CollectionGridComponent } from '../../../widgets/collection-grid/collection-grid.component';
 
 @Component({
   selector: 'app-explore-collections-page',
   standalone: true,
-  imports: [],
+  imports: [CollectionGridComponent],
   templateUrl: './explore-collections-page.component.html',
   styleUrl: './explore-collections-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
