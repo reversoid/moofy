89d151f695913e68bfba40d99c9ef1e380fd3102
reversoid <ancient.gekob@gmail.com>
2024-01-24 23:06:39 +0300

    Create feed page

--- new-frontend/src/pages/feed-page/feed-page.component.ts
+++ new-frontend/src/pages/feed-page/feed-page.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiNotificationModule } from '@taiga-ui/core';
 import { TuiIslandModule } from '@taiga-ui/kit';
+import { CollectionGridComponent } from '../../widgets/collection-grid/collection-grid.component';
 
 @Component({
   selector: 'app-feed-page',
   standalone: true,
-  imports: [TuiIslandModule],
+  imports: [TuiIslandModule, TuiNotificationModule, CollectionGridComponent],
   templateUrl: './feed-page.component.html',
   styleUrl: './feed-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
