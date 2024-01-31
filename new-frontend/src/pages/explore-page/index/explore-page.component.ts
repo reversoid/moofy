35748f17711dbe188ac40003aa696af8451c8444
reversoid <ancient.gekob@gmail.com>
2024-01-31 23:08:10 +0300

    Made component for links group

--- new-frontend/src/pages/explore-page/index/explore-page.component.ts
+++ new-frontend/src/pages/explore-page/index/explore-page.component.ts
@@ -1,7 +1,13 @@
+import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { TuiButtonModule, TuiSvgModule, TuiTextfieldControllerModule } from '@taiga-ui/core';
-import { TuiInputModule, TuiIslandModule, TuiTabsModule } from '@taiga-ui/kit';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { TuiSvgModule, TuiTextfieldControllerModule } from '@taiga-ui/core';
+import { TuiInputModule, TuiTabsModule } from '@taiga-ui/kit';
+import { filter, map } from 'rxjs';
+import {
+  LinkItem,
+  LinksGroupComponent,
+} from '../../../shared/ui/links-group/links-group.component';
 
 @Component({
   selector: 'app-explore-page',
@@ -12,13 +18,30 @@ import { TuiInputModule, TuiIslandModule, TuiTabsModule } from '@taiga-ui/kit';
     TuiSvgModule,
     TuiInputModule,
     TuiTextfieldControllerModule,
-    TuiIslandModule,
-    TuiButtonModule,
+    LinksGroupComponent,
+    AsyncPipe,
   ],
   templateUrl: './explore-page.component.html',
   styleUrl: './explore-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ExplorePageComponent {
+  constructor(private readonly router: Router) {}
+
   activeItemIndex = 0;
+
+  links: LinkItem[] = [
+    { label: 'Пользователи', link: 'profiles' },
+    { label: 'Коллекции', link: 'collections' },
+  ];
+
+  selectedLinkIndex$ = this.router.events.pipe(filter((v) => v instanceof NavigationEnd)).pipe(
+    map((v) => {
+      if (!(v instanceof NavigationEnd)) {
+        return null;
+      }
+      // TODO some logic for returning correct index
+      return null;
+    }),
+  );
 }
