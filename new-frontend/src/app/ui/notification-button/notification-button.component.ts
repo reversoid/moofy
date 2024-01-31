01cea593ba268a875e716f371d10f6257f279cd0
reversoid <ancient.gekob@gmail.com>
2024-01-31 21:53:26 +0300

    Implement badge when some notifications are loaded

--- new-frontend/src/app/ui/notification-button/notification-button.component.ts
+++ new-frontend/src/app/ui/notification-button/notification-button.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TuiButtonModule, TuiDialogService } from '@taiga-ui/core';
 import { NotificationsDialogComponent } from '../../../features/notifications/notifications-dialog/notifications-dialog.component';
 import { PolymorpheusComponent } from '@tinkoff/ng-polymorpheus';
+import { TuiBadgeModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-notification-button',
   standalone: true,
-  imports: [TuiButtonModule],
+  imports: [TuiButtonModule, TuiBadgeModule],
   templateUrl: './notification-button.component.html',
   styleUrl: './notification-button.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
