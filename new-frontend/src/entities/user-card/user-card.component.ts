b49f4f4629b8da2f54d3f32afadd72d251e1c596
reversoid <ancient.gekob@gmail.com>
2024-02-04 14:45:09 +0300

    Fix cards for vertical and horizontal views for user and collection

--- new-frontend/src/entities/user-card/user-card.component.ts
+++ new-frontend/src/entities/user-card/user-card.component.ts
@@ -1,18 +1,29 @@
-import { NgOptimizedImage } from '@angular/common';
+import { NgClass, NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { TuiButtonModule, TuiLinkModule } from '@taiga-ui/core';
 import { TuiIslandModule } from '@taiga-ui/kit';
 import { User } from '../../shared/types';
+import { CardComponent } from '../../shared/ui/card/card.component';
 
 @Component({
   selector: 'app-user-card',
   standalone: true,
-  imports: [NgOptimizedImage, TuiIslandModule, RouterModule, TuiLinkModule, TuiButtonModule],
+  imports: [
+    NgOptimizedImage,
+    TuiIslandModule,
+    RouterLink,
+    TuiLinkModule,
+    TuiButtonModule,
+    CardComponent,
+    NgClass,
+  ],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserCardComponent {
   @Input({ required: true }) user!: User;
+
+  @Input() view?: 'vertical' | 'horizontal' = 'vertical';
 }
--- new-frontend/src/shared/ui/card/card.component.ts
+++ new-frontend/src/shared/ui/card/card.component.ts
@@ -1,17 +1,16 @@
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf, NgTemplateOutlet } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TuiIslandModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [TuiIslandModule, NgClass],
+  imports: [TuiIslandModule, NgClass, RouterLink, NgIf, NgTemplateOutlet],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent {
   @Input() view?: 'vertical' | 'horizontal' = 'vertical';
-
-  @Input() title?: string;
 }
