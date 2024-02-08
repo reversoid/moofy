8e389882dae73e8a61b4a3f084d86c76f9a8e0c9
reversoid <ancient.gekob@gmail.com>
2024-02-08 20:08:14 +0300

    Improve ui

--- new-frontend/src/pages/profile-page/new-personal-collection-page/steps/select-combine-options-step/select-combine-options-step.component.ts
+++ new-frontend/src/pages/profile-page/new-personal-collection-page/steps/select-combine-options-step/select-combine-options-step.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 import { PersonalCollectionData } from '../../new-personal-collection-page.component';
-import { TuiRadioListModule } from '@taiga-ui/kit';
+import { TuiIslandModule, TuiRadioListModule } from '@taiga-ui/kit';
 import { TuiButtonModule } from '@taiga-ui/core';
+import { NgClass } from '@angular/common';
 
 type CombineOptions = Pick<
   NonNullable<PersonalCollectionData['combineOptions']>,
@@ -11,7 +12,7 @@ type CombineOptions = Pick<
 @Component({
   selector: 'app-select-combine-options-step',
   standalone: true,
-  imports: [TuiRadioListModule, TuiButtonModule],
+  imports: [TuiRadioListModule, TuiButtonModule, TuiIslandModule, NgClass],
   templateUrl: './select-combine-options-step.component.html',
   styleUrl: './select-combine-options-step.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -23,16 +24,19 @@ export class SelectCombineOptionsStepComponent {
     {
       name: 'Сохранять',
       description: 'Все коллекции будут сохранены',
+      mood: 'positive',
     },
 
     {
       name: 'Удалить пустые',
       description: 'Если после переноса обзоров, останутся пустые коллекции, то они будут удалены',
+      mood: 'negative',
     },
 
     {
       name: 'Удалить все',
       description: 'Все объединенные коллекции будут удалены',
+      mood: 'negative',
     },
   ];
 
@@ -40,11 +44,13 @@ export class SelectCombineOptionsStepComponent {
     {
       name: 'Скопировать',
       description: 'Обзоры останутся в коллекциях',
+      mood: 'neutral',
     },
 
     {
       name: 'Перенести',
       description: 'Обзоры будут перенесены из коллекций',
+      mood: 'neutral',
     },
   ];
 
@@ -52,11 +58,13 @@ export class SelectCombineOptionsStepComponent {
     {
       name: 'Все с описанием',
       description: 'Все обзоры с описанием будут участвовать в объединении',
+      mood: 'neutral',
     },
 
     {
       name: 'Все с оценкой и описанием',
       description: 'Все обзоры с оценкой и описанием будут участвовать в объединении',
+      mood: 'neutral',
     },
   ];
 }
