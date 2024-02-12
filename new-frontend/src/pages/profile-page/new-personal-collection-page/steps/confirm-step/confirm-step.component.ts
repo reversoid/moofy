539c2b49c0e7fd5f4c65b2b9baa6799e554e133c
reversoid <ancient.gekob@gmail.com>
2024-02-12 18:40:32 +0300

    Create confirm page

--- new-frontend/src/pages/profile-page/new-personal-collection-page/steps/confirm-step/confirm-step.component.ts
+++ new-frontend/src/pages/profile-page/new-personal-collection-page/steps/confirm-step/confirm-step.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TuiButtonModule } from '@taiga-ui/core';
+import { TuiIslandModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-confirm-step',
   standalone: true,
-  imports: [TuiButtonModule],
+  imports: [TuiButtonModule, TuiIslandModule],
   templateUrl: './confirm-step.component.html',
   styleUrl: './confirm-step.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
