b2eab96f4e0ede276809476785b0fc63447a21ce
George Kobunov <georgijkobunov@MacBook-Pro-Georgij-2.local>
2024-03-08 22:23:12 +0300

    Implement explore service

---
+++ new-frontend/src/features/explore/explore.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CollectionWithInfo, PaginatedData, Profile } from '../../shared/types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExploreService {
+  constructor(private readonly http: HttpClient) {}
+
+  searchCollections(search?: string) {
+    const params = new HttpParams();
+    if (search) {
+      params.append('search', search);
+    }
+
+    params.append('limit', 20);
+
+    return this.http.get<PaginatedData<CollectionWithInfo>>('explore/collections', {
+      params,
+    });
+  }
+
+  searchProfiles(search?: string) {
+    const params = new HttpParams();
+    if (search) {
+      params.append('search', search);
+    }
+
+    params.append('limit', 20);
+
+    return this.http.get<PaginatedData<CollectionWithInfo>>('explore/profiles', {
+      params,
+    });
+  }
+
+  getTopProfiles() {
+    return this.http.get<PaginatedData<Profile>>('explore/top-profiles', { params: { limit: 20 } });
+  }
+
+  getUnseenAmount() {
+    return this.http.get<{ amount: number }>('explore/unseen/amount');
+  }
+
+  getUnseenCollections(nextKey?: string) {
+    return this.http.get<PaginatedData<CollectionWithInfo>>('explore/unseen', {
+      params: nextKey ? { nextKey } : undefined,
+    });
+  }
+}
