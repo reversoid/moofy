94d4057e3a71a4395bf6c0142c977321999dfc40
reversoid <ancient.gekob@gmail.com>
2024-03-30 00:44:27 +0300

    Add comment nested style

--- new-frontend/src/entities/comment/comment.component.ts
+++ new-frontend/src/entities/comment/comment.component.ts
@@ -1,15 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { TuiButtonModule, TuiLinkModule } from '@taiga-ui/core';
 import { TuiIslandModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [TuiIslandModule, TuiButtonModule, TuiLinkModule],
+  imports: [TuiIslandModule, TuiButtonModule, TuiLinkModule, NgClass],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CommentComponent {
+  @Input() nested = Math.random() > 0.5;
   // @Input({ required: true }) comment!: CommentWithInfo;
 }
