b81508eb37e5a86334f343b60d06c6592cf71481
reversoid <ancient.gekob@gmail.com>
2024-03-29 22:08:16 +0300

    Implement authorization

--- new-backend/src/modules/image/controller/image.controller.ts
+++ new-backend/src/modules/image/controller/image.controller.ts
@@ -4,17 +4,20 @@ import {
   Post,
   Query,
   Request,
+  UseGuards,
 } from '@nestjs/common';
 import { FastifyRequest } from 'fastify';
 import { IImageController } from './image.controller.interface';
 import { ImageService } from '../service/image.service';
 import { NoImageProvidedException } from '../exceptions/no-image-provided.exception';
 import { ALLOWED_IMAGE_RESOURCES } from '../service/utils';
+import { JwtAuthGuard } from 'src/modules/auth/passport/jwt-auth.guard';
 
 @Controller('image')
 export class ImageController implements IImageController {
   constructor(private readonly imageService: ImageService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('')
   async uploadFile(
     @Request() request: FastifyRequest,
