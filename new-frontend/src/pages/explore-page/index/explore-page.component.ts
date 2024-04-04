cc46cfef72dfb2abe127fb1d948f8eb8c556e678
reversoid <ancient.gekob@gmail.com>
2024-04-04 11:40:06 +0300

    Apply form for explore page options

--- new-frontend/src/pages/explore-page/index/explore-page.component.ts
+++ new-frontend/src/pages/explore-page/index/explore-page.component.ts
@@ -1,5 +1,6 @@
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgFor } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { TuiTextfieldControllerModule } from '@taiga-ui/core';
 import { TuiInputModule, TuiIslandModule, TuiRadioLabeledModule } from '@taiga-ui/kit';
@@ -14,9 +15,24 @@ import { TuiInputModule, TuiIslandModule, TuiRadioLabeledModule } from '@taiga-u
     AsyncPipe,
     TuiIslandModule,
     TuiRadioLabeledModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgFor,
   ],
   templateUrl: './explore-page.component.html',
   styleUrl: './explore-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExplorePageComponent {}
+export class ExplorePageComponent {
+  constructor(private readonly fb: FormBuilder) {}
+
+  searchObjects = [
+    { value: 'users', label: 'Пользователи' },
+    { value: 'collections', label: 'Коллекции' },
+  ];
+
+  exploreOptionsForm = this.fb.group({
+    searchObject: this.fb.control(this.searchObjects[0].value),
+    search: this.fb.control(''),
+  });
+}
