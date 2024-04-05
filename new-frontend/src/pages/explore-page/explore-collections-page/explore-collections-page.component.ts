bcb79fb4cdda41b47c73e1c0c8b3585e7dd4ae13
reversoid <ancient.gekob@gmail.com>
2024-04-05 12:20:49 +0300

    Implement logic for exlore collections page

--- new-frontend/src/pages/explore-page/explore-collections-page/explore-collections-page.component.ts
+++ new-frontend/src/pages/explore-page/explore-collections-page/explore-collections-page.component.ts
@@ -1,5 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CollectionGridComponent } from '../../../widgets/collection-grid/collection-grid.component';
+import { ExplorePageStore } from '../model/explore-page.store';
+import { TuiDestroyService } from '@taiga-ui/cdk';
+import { ExploreService } from '../../../features/explore/explore.service';
+import { combineLatestWith, switchMap, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-explore-collections-page',
@@ -8,5 +12,30 @@ import { CollectionGridComponent } from '../../../widgets/collection-grid/collec
   templateUrl: './explore-collections-page.component.html',
   styleUrl: './explore-collections-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [TuiDestroyService],
 })
-export class ExploreCollectionsPageComponent {}
+export class ExploreCollectionsPageComponent implements OnInit {
+  constructor(
+    private readonly explorePageStore: ExplorePageStore,
+    private readonly destroy$: TuiDestroyService,
+    private readonly exploreService: ExploreService,
+  ) {}
+
+  ngOnInit(): void {
+    this.initHandleSearchChange();
+  }
+
+  private initHandleSearchChange() {
+    this.explorePageStore.search$
+      .pipe(
+        switchMap((search) => {
+          return this.exploreService.searchCollections(search ?? '');
+        }),
+        combineLatestWith(this.explorePageStore.search$),
+        takeUntil(this.destroy$),
+      )
+      .subscribe(([result, search]) => {
+        this.explorePageStore.setCollections({ search, items: result.items });
+      });
+  }
+}
