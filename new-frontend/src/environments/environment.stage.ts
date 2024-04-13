411d6a6602a58d45aa2739eed4f1a71967c8ddc3
reversoid <ancient.gekob@gmail.com>
2024-04-13 22:16:52 +0300

    Fix frontend url

--- new-frontend/src/environments/environment.stage.ts
+++ new-frontend/src/environments/environment.stage.ts
@@ -1,6 +1,6 @@
 import { IEnvironment } from './interface';
 
 export const environment: IEnvironment = {
-  apiUrl: 'https://api.test.moofy.ru',
+  apiUrl: 'https://test.moofy.ru/api',
   production: false,
 };
