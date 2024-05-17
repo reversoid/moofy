810677229d7337b15a3b2451fdd573af9f4e411e
reversoid <ancient.gekob@gmail.com>
2024-05-17 21:16:47 +0300

    Made layout for sidebar

--- new-frontend/src/app/ui/sidebar/sidebar.component.ts
+++ new-frontend/src/app/ui/sidebar/sidebar.component.ts
@@ -1,12 +1,30 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { TuiSidebarModule } from '@taiga-ui/addon-mobile';
+import { TuiButtonModule, TuiLinkModule } from '@taiga-ui/core';
+import { TuiIslandModule, TuiMarkerIconModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [TuiSidebarModule],
+  imports: [
+    TuiSidebarModule,
+    TuiButtonModule,
+    TuiIslandModule,
+    TuiMarkerIconModule,
+    RouterModule,
+    TuiLinkModule,
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  get profileLink() {
+    return ['/profile'];
+  }
+
+  openNotifications() {}
+
+  logout() {}
+}
