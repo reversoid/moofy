2518b93514ec1ee7dab94731a9c1d3f84df621ec
reversoid <ancient.gekob@gmail.com>
2024-08-05 20:57:11 +0300

    Fix deps

--- new-backend/src/main.ts
+++ new-backend/src/main.ts
@@ -1,16 +1,16 @@
+import fastifyCookie from '@fastify/cookie';
+import multipart from '@fastify/multipart';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { patchNestJsSwagger } from 'nestjs-zod';
-import fastifyCookie from '@fastify/cookie';
-import { setupS3 } from './shared/utils/s3/s3';
+import { AppModule } from './app.module';
 import { AppEnvironments } from './config/global.config';
-import { ConfigService } from '@nestjs/config';
-import multipart from '@fastify/multipart';
+import { setupS3 } from './shared/utils/s3/s3';
 
 patchNestJsSwagger();
 
