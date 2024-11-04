6a04c489f110dbd73a0b40cd9589bec699652f0d
reversoid <ancient.gekob@gmail.com>
2024-11-04 19:09:21 +0300

    feat: define interface for film provider

--- v2/packages/core/film-providers/index.ts
+++ v2/packages/core/film-providers/index.ts
@@ -1,1 +1,6 @@
-// TODO
+import { Film } from "../entities/film";
+
+export interface IFilmProvider {
+  searchFilmsByName(name: string): Promise<Film>;
+  getFilmByKpId(id: string): Promise<Film | null>;
+}
