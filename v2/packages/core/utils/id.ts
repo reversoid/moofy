b5fb05330027d3988081f8357851f22d2690792d
reversoid <ancient.gekob@gmail.com>
2024-11-04 20:26:46 +0300

    feat: define id class

--- v2/packages/core/utils/id.ts
+++ v2/packages/core/utils/id.ts
@@ -1,2 +1,23 @@
-// TODO
-export class Id {}
+export class Id {
+  private _value: number;
+
+  constructor(value: number) {
+    this._value = Id.validateId(value);
+  }
+
+  public get value() {
+    return this._value;
+  }
+
+  public static validateId(value: number): number {
+    if (typeof value !== "number") {
+      throw new Error("Id Must Be A Number");
+    }
+
+    return value;
+  }
+
+  toNumber() {
+    return this.value;
+  }
+}
