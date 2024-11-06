6c8ea55bc4e00d030a4eccd95b80b81e0d8e27ff
reversoid <ancient.gekob@gmail.com>
2024-11-06 21:28:29 +0300

    feat: define config

---
+++ v2/packages/config/config.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+import { parseEnv } from "znv";
+import dotenv from "dotenv";
+import { findWorkspaceDir } from "@pnpm/find-workspace-dir";
+import path from "path";
+
+const workspaceDir = await findWorkspaceDir(process.cwd());
+
+if (!workspaceDir) {
+  throw new Error("No workspace directory found. Something wrong happened.");
+}
+
+dotenv.config({ path: path.join(workspaceDir, ".env") });
+
+export default parseEnv(process.env, {
+  UNOFFICIAL_KP_API_KEYS: z.array(z.string()),
+});
