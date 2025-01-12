e09a9e2d7170c723ad057db16cb2d7fc41b16d90
reversoid <ancient.gekob@gmail.com>
2025-01-12 09:39:35 +0800

    feat: init svelte site

---
+++ v2/apps/site/src/lib/shared/ui/button.svelte
@@ -0,0 +1,1 @@
+<button class=""> Button </button>
---
+++ v2/apps/site/src/routes/welcome/collections/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	// You can add more imports as needed
+	import Button from '$lib/shared/ui/button.svelte';
+</script>
+
+<Button></Button>
