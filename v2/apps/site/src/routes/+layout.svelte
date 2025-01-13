8524eacd74c0e83b18ac2a7fe84315371623bde3
reversoid <ancient.gekob@gmail.com>
2025-01-13 16:25:16 +0800

    feat: installed some components

--- v2/apps/site/src/routes/+layout.svelte
+++ v2/apps/site/src/routes/+layout.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
+	import '../fonts.css';
 	import { Header, Footer } from '../app/ui';
 	let { children } = $props();
 </script>
 
-<ModeWatcher />
 <div class="flex h-full flex-col">
 	<Header />
 	<main class="flex-grow">{@render children()}</main>
--- v2/apps/site/src/routes/welcome/collections/+page.svelte
+++ v2/apps/site/src/routes/welcome/collections/+page.svelte
@@ -2,4 +2,5 @@
 	import { Button } from '$lib/components/ui/button';
 </script>
 
+<h1>Hello world!</h1>
 <Button>Login</Button>
