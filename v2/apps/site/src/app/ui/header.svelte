c38a28b81c22948bdec0099b2cb69e22d2f8781d
reversoid <ancient.gekob@gmail.com>
2025-01-13 16:49:04 +0800

    feat: made something like header

--- v2/apps/site/src/app/ui/header.svelte
+++ v2/apps/site/src/app/ui/header.svelte
@@ -1,1 +1,18 @@
-<header class="h-20 bg-slate-700">Some header</header>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { Avatar } from '$lib/components/ui/avatar';
+	import Wrapper from '$lib/ui/wrapper.svelte';
+</script>
+
+<header class="flex h-20 items-center justify-center backdrop-blur-md">
+	<Wrapper class="flex items-center justify-between">
+		<div>
+			<img src="/logo.svg" width="128" height="64" alt="Moofy Logo" />
+		</div>
+
+		<div>
+			<Button>Войти</Button>
+			<!-- <Avatar>GK</Avatar> -->
+		</div>
+	</Wrapper>
+</header>
---
+++ v2/apps/site/src/lib/ui/wrapper.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+
+	const className = $$props['class'];
+	export { className as class };
+</script>
+
+<div class={cn('w-full max-w-screen-2xl px-4 max-sm:px-2', className)}>
+	<slot />
+</div>
