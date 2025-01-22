b6b918b1dda1e1fa85ff20868f45f7927d4df9de
reversoid <ancient.gekob@gmail.com>
2025-01-23 01:41:59 +0700

    feat: make some layout

--- v2/apps/site/src/lib/entities/collection-card/collection-card.svelte
+++ v2/apps/site/src/lib/entities/collection-card/collection-card.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card';
+	import Link from '$lib/ui/link.svelte';
 </script>
 
-<Card.Root>
-	<Card.Header>
-		<Card.Title class="overflow-hidden text-ellipsis whitespace-nowrap">
-			Some collection title
-		</Card.Title>
-		<Card.Description>Some collection description</Card.Description>
-	</Card.Header>
+<Link href="/collections/1">
+	<Card.Root>
+		<Card.Header>
+			<Card.Title class="overflow-hidden text-ellipsis whitespace-nowrap">
+				Some collection title
+			</Card.Title>
+			<Card.Description>Some collection description</Card.Description>
+		</Card.Header>
 
-	<Card.Content>
-		<img
-			class="aspect-[4/3] w-full rounded-md object-cover"
-			src="https://picsum.photos/300/300"
-			alt="Collection"
-		/>
-	</Card.Content>
-</Card.Root>
+		<Card.Content>
+			<img
+				class="aspect-[4/3] w-full rounded-md object-cover"
+				src="https://picsum.photos/300/300"
+				alt="Collection"
+			/>
+		</Card.Content>
+	</Card.Root>
+</Link>
---
+++ v2/apps/site/src/routes/collections/[id]/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+	import * as Avatar from '$lib/components/ui/avatar';
+	import * as Card from '$lib/components/ui/card';
+	import { Textarea } from '$lib/components/ui/textarea';
+	import Heading from '$lib/ui/heading.svelte';
+	import Link from '$lib/ui/link.svelte';
+	import Wrapper from '$lib/ui/wrapper.svelte';
+	import { ReviewsList } from '$lib/widgets/reviews-list';
+</script>
+
+<Wrapper>
+	<Heading>Collection 1</Heading>
+
+	<div class="mt-6 grid grid-cols-[4fr_1fr] gap-2">
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Описание</Card.Title>
+			</Card.Header>
+
+			<Card.Content>
+				<p>Some description</p>
+			</Card.Content>
+		</Card.Root>
+
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>О коллекции</Card.Title>
+			</Card.Header>
+
+			<Card.Content>
+				<ul class="flex flex-col gap-1">
+					<li>Создатель: <Link href="/users/reversoid">reversoid</Link></li>
+					<li>Обновлено: 12.01.2025</li>
+					<li>Приватная коллекция</li>
+				</ul>
+			</Card.Content>
+		</Card.Root>
+	</div>
+
+	<Heading class="mt-6" type="h2">Обзоры</Heading>
+	<ReviewsList />
+</Wrapper>
