6dcd99d2c2d0d4f8e209c4dc472a4286f193dd64
reversoid <ancient.gekob@gmail.com>
2025-01-23 00:11:04 +0700

    feat: some fix colelction card

--- v2/apps/site/src/lib/entities/collection-card/collection-card.svelte
+++ v2/apps/site/src/lib/entities/collection-card/collection-card.svelte
@@ -3,18 +3,18 @@
 </script>
 
 <Card.Root>
-	<Card.Header>
-		<Card.Title>Some collection title</Card.Title>
+	<Card.Header class="max-sm:p-4">
+		<Card.Title class="overflow-hidden text-ellipsis whitespace-nowrap">
+			Some collection title
+		</Card.Title>
 		<Card.Description>Some collection description</Card.Description>
 	</Card.Header>
 
-	<Card.Content>
-		<div>
-			<img
-				class="aspect-[4/3] w-full rounded-md object-cover"
-				src="https://picsum.photos/300/300"
-				alt="Collection"
-			/>
-		</div>
+	<Card.Content class="max-sm:p-4 max-sm:pt-0">
+		<img
+			class="aspect-[4/3] w-full rounded-md object-cover"
+			src="https://picsum.photos/300/300"
+			alt="Collection"
+		/>
 	</Card.Content>
 </Card.Root>
--- v2/apps/site/src/lib/widgets/collections-grid/collections-grid.svelte
+++ v2/apps/site/src/lib/widgets/collections-grid/collections-grid.svelte
@@ -3,7 +3,7 @@
 	import { CollectionCard } from '$lib/entities/collection-card';
 </script>
 
-<div class="grid grid-cols-4 gap-4">
+<div class="grid grid-cols-4 gap-4 max-xl:grid-cols-3 max-md:grid-cols-2 max-sm:gap-2">
 	<CollectionCard />
 	<CollectionCard />
 	<CollectionCard />
