0cd6d9746eaaf60502c4e273299341daac09cee9
reversoid <ancient.gekob@gmail.com>
2025-01-23 00:40:26 +0700

    feat: implement card to create colelctions

---
+++ v2/apps/site/src/lib/features/collection/create-collection-card.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
+	import { IconPlus } from '@tabler/icons-svelte';
+</script>
+
+<Card.Root class="flex flex-col items-center justify-center">
+	<Card.Header>
+		<Card.Title>Новая коллекция</Card.Title>
+	</Card.Header>
+	<Card.Content>
+		<Button>
+			<IconPlus class="mr-1" />
+			Создать
+		</Button>
+	</Card.Content>
+</Card.Root>
