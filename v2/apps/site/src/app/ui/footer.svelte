ee4ad9355969934a87dca4b02c9883f706005b00
reversoid <ancient.gekob@gmail.com>
2025-01-23 01:03:42 +0700

    fix: some fix

--- v2/apps/site/src/app/ui/footer.svelte
+++ v2/apps/site/src/app/ui/footer.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <footer class="flex items-center justify-center">
-	<Wrapper class="flex items-center justify-between gap-3 py-4 max-sm:flex-col">
+	<Wrapper class="flex items-center justify-between gap-3 py-4 pt-5 max-sm:flex-col">
 		<div>Создатель: <Link isExternal href="https://github.com/reversoid">reversoid</Link></div>
 
 		<div class="flex items-center justify-center gap-2">
