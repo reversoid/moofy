764c83092003dd7ee666f6b0be6699a205f061da
reversoid <ancient.gekob@gmail.com>
2025-01-23 00:19:37 +0700

    fix:

--- v2/apps/site/src/lib/entities/collection-card/collection-card.svelte
+++ v2/apps/site/src/lib/entities/collection-card/collection-card.svelte
@@ -3,14 +3,14 @@
 </script>
 
 <Card.Root>
-	<Card.Header class="max-sm:p-4">
+	<Card.Header class="max-sm:p-4 max-sm:pb-0">
 		<Card.Title class="overflow-hidden text-ellipsis whitespace-nowrap">
 			Some collection title
 		</Card.Title>
 		<Card.Description>Some collection description</Card.Description>
 	</Card.Header>
 
-	<Card.Content class="max-sm:p-4 max-sm:pt-0">
+	<Card.Content class="max-sm:p-4">
 		<img
 			class="aspect-[4/3] w-full rounded-md object-cover"
 			src="https://picsum.photos/300/300"
