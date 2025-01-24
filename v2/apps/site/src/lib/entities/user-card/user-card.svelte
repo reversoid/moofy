6a8eedd1dcc8ad022a2ffa9f2439edbcae88d2a3
reversoid <ancient.gekob@gmail.com>
2025-01-24 16:36:18 +0700

    feat: improve users cards

--- v2/apps/site/src/lib/entities/user-card/user-card.svelte
+++ v2/apps/site/src/lib/entities/user-card/user-card.svelte
@@ -5,13 +5,13 @@
 
 <a aria-label={`Profile username`} href="/profiles/username">
 	<Card.Root class="flex flex-col gap-4">
-		<div class="flex flex-row justify-between">
-			<Card.Header>
+		<div class="flex flex-row justify-between gap-4 max-sm:flex-col-reverse max-sm:items-center">
+			<Card.Header class="pr-0 max-sm:px-0 max-sm:pt-0">
 				<Card.Title>John Doe</Card.Title>
 				<Card.Description>Нет описания</Card.Description>
 			</Card.Header>
 
-			<Card.Content class="shrink-0">
+			<Card.Content class="shrink-0 pl-0 max-sm:px-0 max-sm:pb-0">
 				<img class="size-20 rounded-full" src="https://picsum.photos/300/300" alt="User" />
 			</Card.Content>
 		</div>
--- v2/apps/site/src/lib/widgets/users-list/users-list.svelte
+++ v2/apps/site/src/lib/widgets/users-list/users-list.svelte
@@ -11,7 +11,9 @@
 		{/snippet}
 	</Input>
 
-	<div class="grid grid-cols-4 gap-4">
+	<div
+		class="grid grid-cols-4 gap-4 max-xl:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 max-sm:gap-2"
+	>
 		<UserCard />
 		<UserCard />
 		<UserCard />
