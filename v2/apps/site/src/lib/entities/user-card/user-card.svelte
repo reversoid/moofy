7593f5c82b24d1a9c8be4cefe79d9898e071c1f6
reversoid <ancient.gekob@gmail.com>
2025-01-24 16:22:14 +0700

    feat: make users grid

---
+++ v2/apps/site/src/lib/entities/user-card/user-card.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
+</script>
+
+<a aria-label={`Profile username`} href="/profile/username">
+	<Card.Root class="flex flex-col gap-4">
+		<div class="flex flex-row justify-between">
+			<Card.Header>
+				<Card.Title>John Doe</Card.Title>
+				<Card.Description>Нет описания</Card.Description>
+			</Card.Header>
+
+			<Card.Content class="shrink-0">
+				<img class="size-20 rounded-full" src="https://picsum.photos/300/300" alt="User" />
+			</Card.Content>
+		</div>
+
+		<Card.Footer>
+			<Button onclick={(e) => e.preventDefault()} class="w-full">Follow</Button>
+		</Card.Footer>
+	</Card.Root>
+</a>
---
+++ v2/apps/site/src/lib/widgets/users-list/users-list.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { UserCard } from '$lib/entities/user-card';
+</script>
+
+<div class="grid grid-cols-4 gap-4">
+	<UserCard />
+	<UserCard />
+	<UserCard />
+	<UserCard />
+	<UserCard />
+</div>
