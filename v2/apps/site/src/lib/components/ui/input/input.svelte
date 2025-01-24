3cc12f3bac84b83f5e3294e4b9fc3933077a7af4
reversoid <ancient.gekob@gmail.com>
2025-01-24 15:57:05 +0700

    fix: add prefix for input

--- v2/apps/site/src/lib/components/ui/input/input.svelte
+++ v2/apps/site/src/lib/components/ui/input/input.svelte
@@ -13,12 +13,24 @@
 	}: WithElementRef<HTMLInputAttributes> & { inputPrefix?: Snippet } = $props();
 </script>
 
-<input
-	bind:this={ref}
-	class={cn(
-		'border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-		className
-	)}
-	bind:value
-	{...restProps}
-/>
+<div
+	class="focus-within:ring-ring focus-within:ring-offset-background relative flex w-full rounded-md focus-within:ring-2 focus-within:ring-offset-2"
+>
+	{#if inputPrefix}
+		<div
+			class="border-input bg-background text-muted-foreground flex items-center rounded-l-md border-y border-l px-3"
+		>
+			{@render inputPrefix?.()}
+		</div>
+	{/if}
+	<input
+		bind:this={ref}
+		class={cn(
+			'border-input bg-background placeholder:text-muted-foreground flex h-10 w-full rounded-md border px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+			inputPrefix && 'rounded-l-none border-l-0 pl-0',
+			className
+		)}
+		bind:value
+		{...restProps}
+	/>
+</div>
