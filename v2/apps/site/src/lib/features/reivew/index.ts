c54f14267a6bcd600c2eb1a334f5bc934a55fbce
reversoid <ancient.gekob@gmail.com>
2025-01-26 15:48:18 +0700

    fix: import fix

--- v2/apps/site/src/lib/features/reivew/index.ts
+++ v2/apps/site/src/lib/features/reivew/index.ts
@@ -1,2 +1,2 @@
-export { default as EditReview } from './edit-review.svelte';
-export { default as CreateReview } from './create-review.svelte';
+export { default as EditReview } from './ui/edit-review.svelte';
+export { default as CreateReview } from './ui/create-review.svelte';
