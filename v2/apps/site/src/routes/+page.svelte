60fb72cd3762764cc3a60cba397b8ecdf82093d3
reversoid <ancient.gekob@gmail.com>
2025-02-01 02:00:28 +0700

    fix: improve landing

--- v2/apps/site/src/routes/+page.svelte
+++ v2/apps/site/src/routes/+page.svelte
@@ -5,30 +5,32 @@
 	import Wrapper from '$lib/ui/wrapper.svelte';
 </script>
 
-<Wrapper
-	class="bg-muted relative flex min-h-[inherit] flex-col items-center justify-center gap-10  overflow-hidden rounded-3xl"
->
-	<div class="absolute inset-0 z-0 overflow-hidden">
-		<video
-			autoplay
-			loop
-			muted
-			playsinline
-			class="h-full w-full object-cover opacity-30 blur-md brightness-50 grayscale"
-		>
-			<source src="/output.webm" type="video/webm" />
-			<!-- <source src="/background.mp4" type="video/mp4" /> -->
-		</video>
-	</div>
-
-	<div class="z-10 flex flex-col items-center justify-center gap-10">
-		<div class="flex flex-col items-center justify-center gap-5">
-			<Heading>Создавай коллекции фильмов</Heading>
-			<Heading type="h2">и делись</Heading>
+<Wrapper class="min-h-[inherit]">
+	<Wrapper
+		class="bg-muted relative flex min-h-[inherit] flex-col items-center justify-center gap-10 overflow-hidden rounded-3xl"
+	>
+		<div class="absolute inset-0 z-0">
+			<video
+				autoplay
+				loop
+				muted
+				playsinline
+				class="h-full w-full object-cover opacity-30 blur-md brightness-50 grayscale"
+			>
+				<source src="/output.webm" type="video/webm" />
+				<!-- <source src="/background.mp4" type="video/mp4" /> -->
+			</video>
 		</div>
 
-		<Button href="/welcome/collections" size="lg" class="w-56">
-			<span>Начать</span>
-		</Button>
-	</div>
+		<div class="z-10 flex flex-col items-center justify-center gap-10">
+			<div class="flex flex-col items-center justify-center gap-5">
+				<Heading class="text-center">Создавай коллекции фильмов</Heading>
+				<Heading type="h2" class="text-center">и делись</Heading>
+			</div>
+
+			<Button href="/welcome/collections" size="lg" class="w-56">
+				<span>Начать</span>
+			</Button>
+		</div>
+	</Wrapper>
 </Wrapper>
