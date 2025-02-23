898540e894fee7cf6f5b99dbc2fb43e7d13d3c2d
reversoid <ancient.gekob@gmail.com>
2025-02-23 15:58:23 +0700

    try fix

--- apps/site/src/hooks.server.ts
+++ apps/site/src/hooks.server.ts
@@ -1,12 +1,12 @@
 import config from '@repo/config';
-import type { Handle } from '@sveltejs/kit';
+import type { Handle, HandleFetch } from '@sveltejs/kit';
 
 const replaceApiUrl = (url: string) => {
 	return url.replace(/.*\/api/, 'http://site-api:8080');
 };
 
 export const handle: Handle = async ({ event, resolve }) => {
-	console.log('handling requestt');
+	console.log('handling requestt', event.url.toString());
 
 	if (config.ENV === 'development') {
 		// Request is proxied via Vite
@@ -19,12 +19,31 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 		console.log('newRequest', newRequest);
 
-		return await resolve({
+		const result = await resolve({
 			...event,
 			request: newRequest
 		});
+
+		console.log('result', await result.clone().text());
+
+		return result;
 	}
 
 	const response = await resolve(event);
 	return response;
+};
+
+export const handleFetch: HandleFetch = async ({ request, fetch }) => {
+	if (config.ENV === 'development') {
+		// Let Vite handle the proxying in development
+		return fetch(request);
+	}
+
+	const url = new URL(request.url);
+	if (url.pathname.startsWith('/api')) {
+		const newUrl = replaceApiUrl(request.url);
+		request = new Request(newUrl, request);
+	}
+
+	return fetch(request);
 };
