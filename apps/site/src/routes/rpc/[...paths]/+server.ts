d17f239d9ec229be973ec2e83d4fada49182a735
reversoid <ancient.gekob@gmail.com>
2025-03-07 23:18:10 +0700

    try fix

--- apps/site/src/routes/rpc/[...paths]/+server.ts
+++ apps/site/src/routes/rpc/[...paths]/+server.ts
@@ -1,27 +1,26 @@
 import config from '@repo/config';
-import type { RequestEvent } from '../../$types';
+import type { RequestHandler } from '@sveltejs/kit';
 
 /** Replaces /rpc prefix with the given url */
 const replaceRpcUrl = (url: string, withUrl: string) => {
 	return url.replace(/.*\/rpc/, withUrl);
 };
 
-const handleRoute = ({ fetch, request }: RequestEvent) => {
-	console.log('it will be proxied', request.url);
+const handleRoute: RequestHandler = ({ fetch, request }) => {
+	console.log('it will be proxied', request.url, 'accept', request.headers.get('accept'));
 
 	if (config.ENV === 'development') {
 		const transformedRequest = new Request(
 			replaceRpcUrl(request.url, 'http://localhost:8080'),
 			request
 		);
-		return fetch(transformedRequest).finally((data) => {
+		return fetch(transformedRequest).finally(() => {
 			console.log(
 				'it was proxied',
 				request.url,
-				replaceRpcUrl(request.url, 'http://localhost:8080'),
-				data
+				replaceRpcUrl(request.url, 'http://localhost:8080')
 			);
-			return data;
+			return;
 		});
 	}
 
@@ -29,14 +28,9 @@ const handleRoute = ({ fetch, request }: RequestEvent) => {
 		replaceRpcUrl(request.url, 'http://site-api:8080'),
 		request
 	);
-	return fetch(transformedRequest).finally((data) => {
-		console.log(
-			'it was proxied',
-			request.url,
-			replaceRpcUrl(request.url, 'http://site-api:8080'),
-			data
-		);
-		return data;
+	return fetch(transformedRequest).finally(() => {
+		console.log('it was proxied', request.url, replaceRpcUrl(request.url, 'http://site-api:8080'));
+		return;
 	});
 };
 
@@ -45,5 +39,3 @@ export const POST = handleRoute;
 export const PUT = handleRoute;
 export const PATCH = handleRoute;
 export const DELETE = handleRoute;
-export const OPTIONS = handleRoute;
-export const HEAD = handleRoute;
