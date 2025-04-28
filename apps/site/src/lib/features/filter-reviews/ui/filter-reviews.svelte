e2daffd486bf52f41c47aea7bfdcd02ea1430ce2
reversoid <reversoid@MacBook-Pro-reversoid.local>
2025-04-29 01:24:39 +0700

    feat: make some ui for filters

--- apps/site/src/lib/features/filter-reviews/ui/filter-reviews.svelte
+++ apps/site/src/lib/features/filter-reviews/ui/filter-reviews.svelte
@@ -1,9 +1,33 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import * as Dialog from '$lib/components/ui/dialog';
+	import * as Form from '$lib/components/ui/form';
 	import { Input } from '$lib/components/ui/input';
 	import * as Select from '$lib/components/ui/select';
-	import { IconFilter } from '@tabler/icons-svelte';
+	import { IconFilter, IconFilterFilled, IconSearch } from '@tabler/icons-svelte';
+	import { defaults, superForm } from 'sveltekit-superforms';
+	import { zod } from 'sveltekit-superforms/adapters';
+	import { z } from 'zod';
+
+	const schema = z.object({
+		year: z.string().optional(),
+		duration: z.string().optional(),
+		createdAt: z.string(),
+		updatedAt: z.string(),
+
+		type: z.array(z.string()),
+		genres: z.array(z.string()),
+		tags: z.array(z.number())
+	});
+
+	const form = superForm(defaults({}, zod(schema)), {
+		SPA: true,
+		validators: zod(schema)
+	});
+
+	const { enhance, form: formData } = form;
+
+	let areFiltersApplied = $state(true);
 </script>
 
 <!-- TODO -->
@@ -11,49 +35,114 @@
 <Dialog.Root>
 	<Dialog.Trigger>
 		<Button size="icon" variant="outline">
-			<IconFilter />
+			{#if areFiltersApplied}
+				<IconFilterFilled />
+			{:else}
+				<IconFilter />
+			{/if}
 		</Button>
 	</Dialog.Trigger>
 
 	<Dialog.Content>
 		<Dialog.Header>
 			<Dialog.Title>Фильтр обзоров</Dialog.Title>
-			<Dialog.Description>Здесь Вы можете настроить фильтрацию для обзоров</Dialog.Description>
+			<Dialog.Description class="mt-4">
+				Для текстовых полей вы можете использовать следующие форматы:
+				<br /> <b>A</b> для указания точного значения
+				<br /> <b>A-B</b> для указания диапазона
+				<br /> <b>A, B-С, D</b> для указания перечисления
+			</Dialog.Description>
 		</Dialog.Header>
 
-		<div class="mt-4 flex flex-col gap-2">
-			<Input placeholder="Год" />
-			<Input placeholder="Длительность" />
+		<form use:enhance class="mt-2 flex flex-col gap-2">
+			<Form.Field {form} name="year">
+				<Form.Control>
+					{#snippet children({ attrs }: { attrs: any })}
+						<Form.Label>Год</Form.Label>
+						<Input bind:value={$formData.year} placeholder="2024" {...attrs} />
+					{/snippet}
+				</Form.Control>
+			</Form.Field>
 
-			<Select.Root type="single">
-				<Select.Trigger>Тип</Select.Trigger>
-				<Select.Content>
-					<Select.Item value="zipzipzip">Item</Select.Item>
-					<Select.Item value="zipzipzip2">Item3</Select.Item>
-					<Select.Item value="zipzipzip3">Item4</Select.Item>
-				</Select.Content>
-			</Select.Root>
+			<Form.Field {form} name="duration">
+				<Form.Control>
+					{#snippet children({ attrs }: { attrs: any })}
+						<Form.Label>Длительность, мин.</Form.Label>
+						<Input bind:value={$formData.duration} placeholder="90" {...attrs} />
+					{/snippet}
+				</Form.Control>
+			</Form.Field>
 
-			<Select.Root type="single">
-				<Select.Trigger>Жанры</Select.Trigger>
-				<Select.Content>
-					<Select.Item value="zipzipzip">Item</Select.Item>
-					<Select.Item value="zipzipzip2">Item3</Select.Item>
-					<Select.Item value="zipzipzip3">Item4</Select.Item>
-				</Select.Content>
-			</Select.Root>
+			<Form.Field {form} name="type">
+				<Form.Control>
+					{#snippet children({ attrs }: { attrs: any })}
+						<Form.Label>Тип</Form.Label>
+						<Select.Root type="single">
+							<Select.Trigger>Фильм</Select.Trigger>
+							<Select.Content>
+								<Select.Item value="zipzipzip">Item</Select.Item>
+								<Select.Item value="zipzipzip2">Item3</Select.Item>
+								<Select.Item value="zipzipzip3">Item4</Select.Item>
+							</Select.Content>
+						</Select.Root>
+					{/snippet}
+				</Form.Control>
+			</Form.Field>
 
-			<Select.Root type="single">
-				<Select.Trigger>Теги</Select.Trigger>
-				<Select.Content>
-					<Select.Item value="zipzipzip">Item</Select.Item>
-					<Select.Item value="zipzipzip2">Item3</Select.Item>
-					<Select.Item value="zipzipzip3">Item4</Select.Item>
-				</Select.Content>
-			</Select.Root>
+			<Form.Field {form} name="genres">
+				<Form.Control>
+					{#snippet children({ attrs }: { attrs: any })}
+						<Form.Label>Жанры</Form.Label>
+						<Select.Root type="single">
+							<Select.Trigger>Комедия</Select.Trigger>
+							<Select.Content>
+								<Select.Item value="zipzipzip">Item</Select.Item>
+								<Select.Item value="zipzipzip2">Item3</Select.Item>
+								<Select.Item value="zipzipzip3">Item4</Select.Item>
+							</Select.Content>
+						</Select.Root>
+					{/snippet}
+				</Form.Control>
+			</Form.Field>
 
-			<Input placeholder="Дата создания" />
-			<Input placeholder="Дата обновления" />
-		</div>
+			<Form.Field {form} name="tags">
+				<Form.Control>
+					{#snippet children({ attrs }: { attrs: any })}
+						<Form.Label>Теги</Form.Label>
+						<Select.Root type="single">
+							<Select.Trigger>Выбранные теги</Select.Trigger>
+							<Select.Content>
+								<Select.Item value="zipzipzip">Item</Select.Item>
+								<Select.Item value="zipzipzip2">Item3</Select.Item>
+								<Select.Item value="zipzipzip3">Item4</Select.Item>
+							</Select.Content>
+						</Select.Root>
+					{/snippet}
+				</Form.Control>
+			</Form.Field>
+
+			<Form.Field {form} name="createdAt">
+				<Form.Control>
+					{#snippet children({ attrs }: { attrs: any })}
+						<Form.Label>Дата создания</Form.Label>
+						<Input bind:value={$formData.createdAt} placeholder="01.02.2023" {...attrs} />
+					{/snippet}
+				</Form.Control>
+			</Form.Field>
+
+			<Form.Field {form} name="updatedAt">
+				<Form.Control>
+					{#snippet children({ attrs }: { attrs: any })}
+						<Form.Label>Дата обновления</Form.Label>
+						<Input bind:value={$formData.updatedAt} placeholder="01.02.2023" {...attrs} />
+					{/snippet}
+				</Form.Control>
+			</Form.Field>
+		</form>
+
+		<Dialog.Footer class="mt-4">
+			<Button variant="outline">Сбросить</Button>
+			<Button><IconSearch /> Поиск</Button>
+		</Dialog.Footer>
 	</Dialog.Content>
 </Dialog.Root>
