7c8d5f7e24d9624d817bfcc20ff160d83e5e6c6a
reversoid <reversoid@MacBook-Pro-reversoid.local>
2025-07-02 23:26:34 +0300

    left todo

--- apps/site/src/routes/settings/passkey-item.svelte
+++ apps/site/src/routes/settings/passkey-item.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
-	import { IconKey, IconPencil, IconTrash } from '@tabler/icons-svelte';
+	import { IconCheck, IconKey, IconPencil, IconTrash } from '@tabler/icons-svelte';
+	import { tick } from 'svelte';
 
 	type Props = {};
 </script>
 
+<!-- TODO open dialog -->
 <div class="flex items-center justify-between gap-4 px-4 py-3">
 	<div class="flex flex-col gap-2">
 		<div class="flex items-center gap-2">
-			<IconKey size={20} /> <span>Passkey 2</span>
+			<IconKey size={20} />
+
+			<span>Passkey 2</span>
 		</div>
 		<span class="text-muted-foreground text-sm">Создано 10.11.2025</span>
 	</div>
